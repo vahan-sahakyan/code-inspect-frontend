8ba25349709d158dde78635d6d554f8a054f40b8
vahan-sahakyan <vahan.sahakyan1996@gmail.com>
2024-09-02 00:01:00 +0400

    http -> https

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const VITE_BACKEND_URL =
-  (import.meta.env.VITE_BACKEND_URL && `http://${import.meta.env.VITE_BACKEND_URL}`) || `http://${location.hostname}`;
+  (import.meta.env.VITE_BACKEND_URL && `https://${import.meta.env.VITE_BACKEND_URL}`) || `http://${location.hostname}`;
 const VITE_BACKEND_PORT = import.meta.env.VITE_BACKEND_PORT || '8080';
 
 const backendUrl = `${VITE_BACKEND_URL}:${VITE_BACKEND_PORT}`;
